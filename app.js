ed3929a77d67849bbe03720e3eb886638a8c9a08
designeralabi <bdlkrmalabi@gmail.com>
2022-01-16 00:56:55 +0100

    user input with two data binding

---
+++ app.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: ''
+    };
+  },
+  methods: {
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    }
+  }
+});
+
+app.mount('#events');
