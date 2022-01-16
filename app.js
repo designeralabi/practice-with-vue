5f5ef9f55945bf02e992b01d2d03898f6046cbfd
designeralabi <bdlkrmalabi@gmail.com>
2022-01-16 03:40:56 +0100

    prac-with :Class binding

--- app.js
+++ app.js
@@ -1,42 +1,23 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
-      name: ''
+      boxASelected: false,
+      boxBSelected: false,
+      boxCSelected: false
     };
   },
   methods: {
-    add(num) {
-      this.counter = this.counter + num;
-    },
-    reduce(num) {
-      this.counter = this.counter - num;
-      // this.counter--;
-    },
-    reset(){
-      this.name = '';
-    }
-  },
-  watch:{
-    counter(value){
-        if (value > 50){
-          this.counter = 0;
-        }else{
-
-        }
-    }
-  },
-  computed:{
-    fullName(){
-      // prints how many time code runs
-      console.log('running.....');
-      //if the name is empty reset fullName to empty
-      if(this.name === ''){
-        return '';
+    selectedBox(box){
+      if (box === 'A'){
+         this.boxASelected = !this.boxASelected;
+      }else if(box === 'B'){
+        this.boxBSelected = !this.boxBSelected;
+      }else{
+        this.boxCSelected = !this.boxCSelected;
       }
-      return this.name + ' ' + 'DOE';
+     
     }
-  }
+  },
 });
 
-app.mount('#events');
+app.mount("#styling");
