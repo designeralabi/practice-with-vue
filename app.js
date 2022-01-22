686a9bc7f05a612aacf90eb31c67bd07a0def544
designeralabi <bdlkrmalabi@gmail.com>
2022-01-22 17:45:49 +0100

    pratice with refs

--- app.js
+++ app.js
@@ -1,25 +1,55 @@
 const app = Vue.createApp({
   data() {
-    return { 
-      enteredGoalValue: '',
-      goals: []
-     };
-  },
-  computed:{
-    checker(){
-      if (this.enteredGoalValue !== ''){
-        return true;
-      }
-    }
+    return {
+      currentUserInput: '',
+      message: 'Vue is great!',
+    };
   },
   methods: {
-    addGoal() {
-      this.goals.push(this.enteredGoalValue);
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
     },
-    removeList(index){
-      this.goals.splice(index, 1)
-    }
+    setText() {
+      // this.message = this.currentUserInput;
+      this.message = this.$refs.userText.value;
+      // console.dir(this.$refs.userText);
+    },
+  },
+});
+
+app.mount('#app');
+
+const app2 = Vue.createApp({
+  template: `
+    <p>{{ favoriteMeal }}</p>
+  `,
+  data() {
+    return {
+      favoriteMeal: 'Pizza'
+    };
   }
 });
 
-app.mount('#user-goals');
+app2.mount('#app2');
+
+// ....
+
+const data = {
+  message: 'Hello!',
+  longMessage: 'Hello! World!'
+};
+
+const handler = {
+  set(target, key, value) {
+    if (key === 'message') {
+      target.longMessage = value + ' World!';
+    }
+    target.message = value;
+  }
+};
+
+const proxy = new Proxy(data, handler);
+
+proxy.message = 'Hello!!!!';
+
+console.log(proxy.longMessage);
